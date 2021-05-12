95c0ec9cc67ff376ebe9b0644cbb2176be156aa2
MiaFate <52300099+MiaFate@users.noreply.github.com>
2021-05-12 04:39:15 -0300

    _prefix

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		client.user.setActivity("!help - se vienen cositas pronto :sunglasses: ", { type: "PLAYING"})
+		client.user.setActivity("_help - se vienen cositas pronto :sunglasses: ", { type: "PLAYING"})
 		//client.user.setActivity('<activity>');
 		//client.user.setStatus('asd');
 		console.log(`Ready! Logged in as ${client.user.tag}`);
